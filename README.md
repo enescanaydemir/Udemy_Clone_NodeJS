fb530e6619875ac89ad3478d7043bdb94d3ee963
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-04-23 14:59:26 +0300

    Basic preparations

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 "# Udemy_Clone_NodeJS" 
 
 Sistem Analizi ve Tasarımı ders Projesi
+
+Site Adı : Kebo
+Amaç : Adminlerin ve Öğretmenlerin giriş yapabildiği, eğitim videoları payalaşabildikleri bir uygulama
+
+Kullanlan Yazılımlar ve Teknolojiler: Node.js - JavaScript - MongoDB - Postman
--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs") //template engine'imizin ejs olduğunu belirttik
+
+
+//Middlewares
+app.use(express.static("public")) //statik dosyalarımızı belirttik
+
+
+//ROUTES(YÖNLENDİRMELER)
 app.get('/', (req, res) => {
-    // res.send('INDEX SAYFASI') //cevap(response) olarak INDEX SAYFASI döndürdük
-    res.status(200).send('INDEX SAYFASI')
+    res.status(200).render('index', {
+        page_name: "index"
+    })
 })
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        page_name: "about"
+    })
+})
+
 
 const port = 3000;
 app.listen(port, () => {
