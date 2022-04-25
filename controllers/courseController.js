1dc5981da0e09775468db75193606779522a281c
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-04-25 18:20:06 +0300

    courses

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -2,17 +2,36 @@ const Course = require('../models/Course')
 
 //new course create function
 exports.createCourse = async(req, res) => {
-    const course = await Course.create(req.body)
 
     try { //hata yakalamak için try-catch kullandık
+        const course = await Course.create(req.body)
+
         res.status(201).json({ // Bu kısumda postman üzerinden yaptığımız data testinde bize geri dönüş sağlaması için bir kontrol yazdık. 
             status: 'success',
             course
         })
-    } catch { //yanlış ise data testinde burası çıkacak.
+    } catch (error) { //yanlış ise data testinde burası çıkacak.
         res.status(400).json({
             status: 'fail',
             error
+        })
+    }
+}
+
+//Tüm kursları sıralama
+exports.getAllCourses = async(req, res) => {
+
+    try {
+        const courses = await Course.find();
+
+        res.status(200).render('courses', {
+            courses,
+            page_name: 'courses',
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
         })
     }
 }
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 //yeni bir kurs oluşturmak için yapmamız gereken post request göndermek olacak çünkü form dolduracağız
 router.route('/').post(courseController.createCourse)
+router.route('/').get(courseController.getAllCourses) //kursları listeleyeceğimiz için .get ile çağırdık
 
 
 module.exports = router
