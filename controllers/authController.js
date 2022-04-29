9e35ba106d1bf827dba6acbb42c6c901400d040f
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-04-30 01:04:15 +0300

    login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 // Bu sayfada, Yeni bir user oluşturma işlemini fonksiyonel olarak ayarlayacağız. Bu fonksiyonu çalıştırmak için 'userRoute.js' adında bir route oluşturduk
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 exports.createUser = async(req, res) => {
     try {
@@ -8,6 +9,27 @@ exports.createUser = async(req, res) => {
         res.status(201).json({
             status: 'success',
             user,
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        })
+    }
+}
+
+exports.loginUser = (req, res) => {
+    try {
+        const { email, password } = req.body //email ve pass'ı body den yakaladık
+
+        User.findOne({ email: email }, (err, user) => { //emailin hangi user'a ait olduğunu bulduk. datadan gelen email ile login ekranından gelen emaili eşitledik aslında burda ({email: email}) olarak yazdık ancak aynı oldukları için sadece ({email}) yazmamız yeterli oluyor.
+            if (user) { //bu koşul satırı içerisinde "kullanıcı var ise kullanıcının şifresi ile girilen şifrenin eşit olup olmadığını" kontrol ettik. Ayrıca bunun için bcrypt'den faydalandık
+                bcrypt.compare(password, user.password, (err, same) => { //compare() = bcrypt'nin karşılaştırma fonksiyonu. İçerisinde parametre olarak ilk parametre girilen password, ikinci parametre olarakta datada bulunan user'ın password kısmını karşılaştırdık
+                    if (same) {
+                        res.status(200).send('YOU ARE LOGED IN') //Bu kısımda kullanıcı giriş yaptıktan sonra kullanıcının profiline yönlendirilicek ancak şuan o ekran olmadığı için yani USER SESSION olmadığı için şimdilik mesaj gönderiyoruz
+                    }
+                })
+            }
         })
     } catch (error) {
         res.status(400).json({
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,18 +1,26 @@
+// Sayfa gösterimi dosyası
+
 exports.getIndexPage = (req, res) => {
     res.status(200).render('index', {
-        page_name: "index"
+        page_name: "index" //sayfa başlığı
     })
 }
 
 
 exports.getAboutPage = (req, res) => {
     res.status(200).render('about', {
-        page_name: "about"
+        page_name: "about" //sayfa başlığı
     })
 }
 
 exports.getRegisterPage = (req, res) => {
     res.status(200).render('register', {
-        page_name: 'register'
+        page_name: 'register' //sayfa başlığı
+    })
+}
+
+exports.getLoginPage = (req, res) => {
+    res.status(200).render('login', {
+        page_name: 'login' //sayfa başlığı
     })
 }
